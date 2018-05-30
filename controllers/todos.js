681caa5580f8fdfe7b91a996c30877d3244af93a
jrezuke <j.rezuke@verizon.net>
2018-05-30 16:28:30 -0400

    working on pg insert

--- controllers/todos.js
+++ controllers/todos.js
@@ -12,11 +12,23 @@ router.get('/', function (req, res) {
     });
 });
 
-router.post('/new', function(req, res){
-    console.log(`req.body: ${req.body.test}`);
-    db.none('insert into todo(id, title, started, completed, archived) ' +
-            `values( "nextval('todo_id_seq'"), "test", "", "", "", "")`)
-    res.send('added new todo');
+router.post('/new', function(req, res, next){
+    //console.log(`req.body: ${req.body.test}`);
+    let sql = 'INSERT INTO todo(id, title, started, completed, archived)';
+    let values = 'VALUES(nextval(\'todo_id_seq\'),$1,$2,null,null)';
+    //let sql='INSERT INTO simple(id, name) VALUES(nextval(\'todo_id_seq\'), $1,$2,null,null)';
+    db.none(sql, [req.body.title, req.body.started])
+            .then(function () {
+                res.status(200)
+                  .json({
+                    status: 'success',
+                    message: 'Inserted one puppy'
+                  });
+              })
+              .catch(function (err) {
+                return next(err);
+              });
+    
 })
 
 // About page route.
