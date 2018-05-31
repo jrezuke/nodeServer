b23432e864cddf5b4e5f0e8da031baffa405ab87
jrezuke <j.rezuke@verizon.net>
2018-05-31 15:42:29 -0400

    cleanup

--- controllers/todos.js
+++ controllers/todos.js
@@ -13,11 +13,9 @@ router.get('/', function (req, res) {
 });
 
 router.post('/new', function(req, res, next){
-    //console.log(`req.body.title: ${req.body.title}`);
-    //console.log(`req.body.started: ${req.body.started}`);
+    
     let sql = 'INSERT INTO todo(id, title, started, completed, archived)';
     let values = 'VALUES(nextval(\'todo_id_seq\'), $1, $2, null, null) RETURNING id';
-    //let sql='INSERT INTO simple(id, name) VALUES(nextval(\'todo_id_seq\'), $1,$2,null,null)';
     
     db.one(sql + values, [req.body.title, req.body.started])
         .then(data => {
